7eef4b161ea80cffc7ec75d773b68002693ede7e
Mohamed Dridi <125401114+midooos@users.noreply.github.com>
2025-11-19 23:04:02 +0100

    Add files via upload

---
+++ ex4 tp2/scc.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+class scc {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        System.out.print("X = ");
+        int x = clavier.nextInt();
+        System.out.print("Y = ");
+        int y = clavier.nextInt();
+        System.out.println(Math.max(x,y));
+        
+    }
+}
