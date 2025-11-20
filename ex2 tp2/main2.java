f6f80b3063da4c1d3de455eb232cd9ffc6335126
Mohamed Dridi <125401114+midooos@users.noreply.github.com>
2025-11-20 15:54:07 +0100

    Add files via upload

---
+++ ex2 tp2/main2.java
@@ -0,0 +1,28 @@
+
+public class main2 {
+
+    public static void main(String[] args) {
+        int x =1;
+        //int y ;
+        //int z ;
+        int somme=0;
+        //int compteur ;
+        //int total ;
+        //int q;
+        //int diviseur;
+        while (x < 10) {
+            somme=somme+x;
+            x++;
+        }
+       // z = x++ + y;
+        //if (compteur >= 10) {
+           // System.out.println("x est superieur ou egal a 10");
+        //}
+        //total =total - --x;
+        //q = q % diviseur;
+        //somme=somme+x;
+        System.out.println("la somme est " + somme);
+
+    }
+
+}
